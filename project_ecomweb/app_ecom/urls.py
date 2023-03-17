13904b62296f3cd00e0091a1eef78be66780b6fa
sixdgt <c4crypt@gmail.com>
2023-03-17 08:02:25 +0545

    form, product index, product add & dashboard page design

--- project_ecomweb/app_ecom/urls.py
+++ project_ecomweb/app_ecom/urls.py
@@ -2,4 +2,8 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('dashboard/', views.dashboard_index, name='dashboard'),
+
+    # products
+    path('product/add/', views.product_add, name='add-product'),
+    path('products/', views.product_index, name='list-product'),
 ]
--- project_ecomweb/app_ecom/views.py
+++ project_ecomweb/app_ecom/views.py
@@ -1,6 +1,17 @@
 from django.shortcuts import render
+from .forms import ProductAddForm
 
 # Create your views here.
 
+# dashboard
 def dashboard_index(request):
     return render(request, 'dashboard/dashboard.html')
+
+# products
+def product_add(request):
+    add_product = ProductAddForm()
+    context = {"form": add_product}
+    return render(request, 'products/product_add.html', context)
+
+def product_index(request):
+    return render(request, 'products/product_index.html')
