8bb558fe7da0f580b028522c98c22399ca06700d
sixdgt <c4crypt@gmail.com>
2023-03-19 08:09:47 +0545

    add category & add product

--- project_ecomweb/app_ecom/urls.py
+++ project_ecomweb/app_ecom/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
 from . import views
+
 urlpatterns = [
     path('dashboard/', views.dashboard_index, name='dashboard'),
 
+    # categories
+    path('category/add/', views.category_add, name='add-category'),
+    
     # products
     path('product/add/', views.product_add, name='add-product'),
     path('products/', views.product_index, name='list-product'),
--- project_ecomweb/app_ecom/views.py
+++ project_ecomweb/app_ecom/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
-from .forms import ProductAddForm
+from django.shortcuts import render, redirect
+from .forms import ProductAddForm, CategoryAddForm
+from .models import Category, Product
+from django.contrib.auth.models import User
 
 # Create your views here.
 
@@ -7,10 +9,38 @@ from .forms import ProductAddForm
 def dashboard_index(request):
     return render(request, 'dashboard/dashboard.html')
 
+# categories
+def category_add(request):
+    form = CategoryAddForm()
+    context = {"form": form}
+
+    if request.method == "POST":
+        catgry = Category()
+        catgry.category_name = request.POST.get('category_name')
+        catgry.save()
+
+    return render(request, 'categories/add_category.html', context)
+
 # products
 def product_add(request):
     add_product = ProductAddForm()
     context = {"form": add_product}
+
+    if request.method == "POST":
+        category = Category.objects.get(id=request.POST.get('category'))
+        user = User.objects.get(username='puppet')
+
+        product = Product()
+        product.title = request.POST.get('title')
+        product.desc = request.POST.get('desc')
+        product.price = request.POST.get('price')
+        product.quantity = request.POST.get('quantity')
+        product.discount = request.POST.get('discount')
+        product.cod = request.POST.get('cod')
+        product.category = category
+        product.user = user
+        product.save()
+        return redirect('list-product')
     return render(request, 'products/product_add.html', context)
 
 def product_index(request):
