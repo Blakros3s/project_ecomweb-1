dc9b18fd935b2b19c757e4f879e0445a0cf9b106
sixdgt <c4crypt@gmail.com>
2023-03-22 08:03:13 +0545

    product update

--- project_ecomweb/app_ecom/urls.py
+++ project_ecomweb/app_ecom/urls.py
@@ -10,6 +10,7 @@ urlpatterns = [
     # products
     path('product/add/', views.product_add, name='add-product'),
     path('products/', views.product_index, name='list-product'),
+    path('product/update/', views.product_update, name='update-product'),
     path('product/view/<int:id>/', views.product_view, name='view-product'),
     path('product/edit/<int:id>/', views.product_edit, name='edit-product'),
     path('product/delete/<int:id>/', views.product_delete, name='delete-product'),
--- project_ecomweb/app_ecom/views.py
+++ project_ecomweb/app_ecom/views.py
@@ -55,10 +55,28 @@ def product_view(request, id):
 
 def product_edit(request, id):
     product = Product.objects.get(id=id)
-    context = {"data": product}
+    categories = Category.objects.all()
+    context = {"data": product, "categories": categories}
     return render(request, 'products/product_edit.html', context)
 
 def product_delete(request, id):
     product = Product.objects.get(id=id)
     product.delete()
     return redirect("list-product")
+
+def product_update(request):
+    if request.method == "POST":
+        category = Category.objects.get(id=request.POST.get('category'))
+        product = Product.objects.get(id=request.POST.get('id'))
+        user = User.objects.get(username='puppet')
+        product.title = request.POST.get('title')
+        product.desc = request.POST.get('desc')
+        product.cod = request.POST.get('cod')
+        product.price = request.POST.get('price')
+        product.quantity = request.POST.get('quantity')
+        product.discount = request.POST.get('discount')
+        product.category = category
+        product.user = user
+        product.save()
+        return redirect('list-product')
+    return redirect('list-product')
